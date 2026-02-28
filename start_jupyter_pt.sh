9579c5499e874358a129fd885a998283ea08e951
Liam Ernst <wernst24@gmail.com>
2026-02-27 23:57:37 -0500

    add wk7 assignment

---
+++ start_jupyter_pt.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+JUP=~/miniconda3/envs/mlia-pt/bin/jupyter
+
+TS_IP="$(tailscale ip -4 | head -n1)"
+
+exec "$JUP" notebook \
+  --no-browser \
+  --ip="$TS_IP" \
+  --port=8888 \
+  --NotebookApp.port_retries=0
+
